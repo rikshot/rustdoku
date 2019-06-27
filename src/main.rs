8a1a2b749ba13787d09e7ee534eb16920a09812b
rikshot <ville@orkas.fi>
2019-06-27 18:39:34 +0300

    Split using lines()

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use rustdoku::sudoku::solver::brute_force;
 
 fn main() {
 	let sudoku_file = std::fs::read_to_string("sudoku17").expect("sudoku17 file not found");
-	let sudokus: Vec<&str> = sudoku_file.split('\n').collect();
+	let sudokus: Vec<&str> = sudoku_file.lines().collect();
 	for sudoku in sudokus {
 		let mut grid = sudoku.parse().expect("could not parse sudoku");
 		brute_force(&mut grid);
