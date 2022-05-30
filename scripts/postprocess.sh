90fa4da5e2c85d9a9f6b4e8e34c7707bdb22d8b1
Ville Orkas <ville@orkas.fi>
2022-05-30 16:00:53 +0300

    Minify JS

--- scripts/postprocess.sh
+++ scripts/postprocess.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
+terser "$TRUNK_STAGING_DIR"/rustdoku-game.js -c -m --module --output "$TRUNK_STAGING_DIR"/rustdoku-game.js
+
 gzip --best --keep "$TRUNK_STAGING_DIR"/*{.js,.wasm}
 brotli --best --keep "$TRUNK_STAGING_DIR"/*{.js,.wasm}
